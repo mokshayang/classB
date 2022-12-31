c6162ef4e00e56bb6b0dc2579e66601ab05a51a0
unknown <dullstart.yang.com>
2023-01-01 01:29:48 +0800

    練錯了

--- front/marquee.php
+++ front/marquee.php
@@ -1,2 +1,3 @@
 <marquee scrolldelay="120" direction="left" scrollamount="15" style="position:absolute; width:100%; height:40px;">
+重新練習切版中!!
 </marquee>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once "./api/base.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,9 +7,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<title>卓越科技大學校園資訊系統</title>
-	<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-	<script src="./home_files/jquery-1.9.1.min.js"></script>
-	<script src="./home_files/js.js"></script>
+	<link href="./css/css.css" rel="stylesheet" type="text/css">
+	<script src="./js/jquery-1.9.1.min.js"></script>
+	<script src="./js/js.js"></script>
 </head>
 
 <body>
@@ -31,8 +32,8 @@
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 :
-						1 </span>
+					<span class="t">進站總人數 : <?=$total['total']?></span>
+						</span>
 				</div>
 			</div>
 			<?php
@@ -90,7 +91,7 @@
 		</div>
 		<div style="clear:both;"></div>
 		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"></span>
+			<span class="t" style="line-height:123px;"><?=$bottom['bottom']?></span>
 		</div>
 	</div>
 
