d39f5dd9446db9aea94d3c951b65232b690b9a2a
unknown <dullstart.yang.com>
2023-01-03 17:15:21 +0800

    pratived

--- api/bt.php
+++
@@ -1,6 +0,0 @@
-<?php include_once   "base.php";
-$table=$_POST['table'];
-$row = $$table->find(1);
-$row[lcfirst($table)]=$_POST[lcfirst($table)];
-$$table->save($row);
-to("../admin.php?do=".lcfirst($table));
