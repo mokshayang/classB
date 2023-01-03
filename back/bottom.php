bdee109d5e3040e972b7ec127b7f634c3942ad89
unknown <dullstart.yang.com>
2023-01-04 00:58:32 +0800

    bottom && total nice

--- back/bottom.php
+++ back/bottom.php
@@ -1,12 +1,12 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">頁尾版權管理</p>
-    <form method="post"  action="./api/edit.php">
+    <form method="post"  action="./api/bt.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
                     <td width="50%">頁尾版權資料</td>
                     <td width="50%">
-                        <input type="text" value="<?=$bottom['bottom']?>">
+                        <input type="text" name="bottom" value="<?=$bottom['bottom']?>">
                     </td> 
                 </tr>              
             </tbody>
